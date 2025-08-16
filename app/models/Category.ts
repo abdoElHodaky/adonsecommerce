51f2eb04b04f5311ad0e65f14b8f3ce6331a5cca
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-16 20:04:30 +0300

    Update Category.ts

--- app/models/Category.ts
+++ app/models/Category.ts
@@ -1,7 +1,6 @@
 import { DateTime } from 'luxon'
 import { BaseModel, belongsTo, column, hasMany, manyToMany } from '@adonisjs/lucid/orm'
-import type { HasOne,HasMany,BelongsTo , ManyToMany } from 'adonisjs/lucid/types/relations'
-
+import type { HasOne,HasMany,BelongsTo , ManyToMany } from '@adonisjs/lucid/types/relations'
 import Product from './Product.js'
 
 export default class Category extends BaseModel {
