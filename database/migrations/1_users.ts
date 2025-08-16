b6f5f29550c0dd9cadf39a56d3bf2d2963dbbeb7
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-16 22:26:12 +0300

    Update 1_users.ts

--- database/migrations/1_users.ts
+++ database/migrations/1_users.ts
@@ -9,7 +9,7 @@ export default class extends BaseSchema {
       table.string('first_name').notNullable()
       table.string('last_name').notNullable()
       table.string('email').notNullable().unique()
-      table.string('password').notNullable()
+      table.string('pass_word')
       table.string('phone').nullable()
       table.string('avatar').nullable()
       table.enum('user_type', ['admin', 'merchant', 'customer']).defaultTo('customer')
