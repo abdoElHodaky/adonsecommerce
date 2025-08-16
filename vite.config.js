02bf7232fe74cfbeead61b5023367bbede17872e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-16 23:13:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import adonisjs from '@adonisjs/vite/client';
 export default defineConfig({
+    server:{
+        host:"0.0.0.0",
+        allowedHosts:true 
+    },
     plugins: [
         adonisjs({
             entrypoints: ['resources/css/app.css', 'resources/js/app.js'],
