390d3f935029ff12bd219cba2a4933a471e7d433
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-16 19:33:40 +0300

    Update User.ts

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany, HasMany, hasOne, HasOne } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany, hasOne } from '@adonisjs/lucid/orm'
+import type { HasOne,HasMany,BelongsOne } from 'adonisjs/lucid/types/relations'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import hash from '@adonisjs/core/services/hash'
 import Merchant from './Merchant.js'
