925b50c3654a8d7a45710cf6ba1db4b5e7564892
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-16 20:04:59 +0300

    Update Merchant.ts

--- app/models/Merchant.ts
+++ app/models/Merchant.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon'
 import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
-import type { HasOne,HasMany,BelongsTo } from 'adonisjs/lucid/types/relations'
+import type { HasOne,HasMany,BelongsTo } from '@adonisjs/lucid/types/relations'
 
 import User from './User.js'
 import Product from './Product.js'
