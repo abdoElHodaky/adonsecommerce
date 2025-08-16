9906996cd3b76b16bf566f81986319721a0f73d8
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-16 20:07:03 +0300

    Update ProductVariant.ts

--- app/models/ProductVariant.ts
+++ app/models/ProductVariant.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon'
 import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
-import type { HasOne,HasMany,BelongsTo } from 'adonisjs/lucid/types/relations'
+import type { HasOne,HasMany,BelongsTo } from '@adonisjs/lucid/types/relations'
 
 import Product from './Product.js'
 
