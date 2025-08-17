60681394c8ef61ef3f9f97eafe94be95cd09ed9b
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-17 17:42:04 +0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,5 @@
 import { middleware } from './kernel.js'
 import router from '@adonisjs/core/services/router';
 router.get("/home", ctx =>{
-   return "78"
-})
+   return ctx.view.render("pages/welcomer")
+}).use(middleware.view)
