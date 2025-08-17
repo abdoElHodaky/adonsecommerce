ca606d93d03c82a51855eaee950749c7d9e4d951
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-17 17:54:26 +0300

    Update view_middleware.ts

--- app/middleware/view_middleware.ts
+++ app/middleware/view_middleware.ts
@@ -3,6 +3,13 @@ import { HttpContext } from '@adonisjs/core/http'
 import { NextFn } from '@adonisjs/core/types/http'
 import { inject } from '@adonisjs/core'
 import { Edge } from 'edge.js'
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * Middleware to bind the view service to the HttpContext
@@ -22,7 +29,14 @@ export default class ViewMiddleware {
       enumerable: true,
       configurable: true,
     })
-
+    const viewsPath = join(__dirname, '../../resources/views');
+    console.log('Mounting views path:', viewsPath);
+    ctx["view"].mount(viewsPath);
+    
+    console.log('Adding global helpers...');
+    ctx["view"]('currentYear', () => new Date().getFullYear());
+    //ctx["view"].global('appUrl', (path) => `http://localhost:3333${path}`);
+   
     /**
      * Call the next middleware
      */
