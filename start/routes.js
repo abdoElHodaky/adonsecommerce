7bca76ea4634d2855704015e80ee90462ba1b3d9
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-17 13:10:02 +0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,5 @@
 import router from '@adonisjs/core/services/router';
 router.get("/", ctx =>{
- return  ctx["view"].render('welcomer')
+  console.log(ctx)
+ //return  ctx["view"].render('welcomer')
 })
