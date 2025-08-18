a04e9b7a3edceb474558dbc3c67e8874a451e38e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-18 11:38:10 +0300

    Update test-view.js

--- test-view.js
+++ test-view.js
@@ -78,7 +78,7 @@ async function testViewRendering() {
     
     // Try to render the test page
     console.log('Rendering test view...');
-    const html = await edge.render('pages/welcomer');
+    const html = await edge.render('pages/home');
     
     console.log('View rendered successfully!');
     console.log('HTML output length:', html.length);
