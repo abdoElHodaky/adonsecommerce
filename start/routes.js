760fb24da78d3930a0ba53988e83b3311a6f1c08
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-19 07:49:19 +0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -2,21 +2,9 @@ import { middleware } from './kernel.js'
 import router from '@adonisjs/core/services/router'
 import { Edge } from "edge.js"
 import { join } from 'node:path'
-/*// Import route modules
-import homeRoutes from './routes/routes/home.js'
-import authRoutes from './routes/routes/auth.js'
-import adminRoutes from './routes/routes/admin.js'
-import merchantRoutes from './routes/routes/merchant.js'
-import customerRoutes from './routes/routes/customer.js'
-import storeRoutes from './routes/routes/store.js'
-import productRoutes from './routes/routes/product.js'
-import categoryRoutes from './routes/routes/category.js'
-import cartRoutes from './routes/routes/cart.js'
-import paymentRoutes from './routes/routes/payment.js'
-import notificationRoutes from './routes/routes/notification.js'
-import pagesRoutes from './routes/routes/pages.js'
-*/
+
 import  "./routes/home.js";
+import "./routes/auth.js"
 const edge=new Edge()
 edge.mount(join(process.cwd(), 'resources', 'views'))
 // Main route for the homepage
