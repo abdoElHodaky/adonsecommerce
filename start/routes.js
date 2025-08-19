f950e150665405216d95cc2a302d738d4263177a
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2025-08-19 15:54:58 +0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -5,36 +5,6 @@ import { join } from 'node:path'
 
 import  "./routes/store.js";
 import "./routes/auth.js"
-const edge=new Edge()
-edge.mount(join(process.cwd(), 'resources', 'views'))
-// Main route for the homepage
-//router.get("/", async({view}) => {
-/*  try {
-  const html=await edge.render("pages/about/index")
-router.get("/", async({response}) => {
-  try {
-   const html=await edge.renderSync("pages/about/index")
-   //return await response.send(e)
-    // response.safeHeader("Content-Type","text/html")
-     return response.send(html)
-      
-   }catch(error){
-      console.log(error)
-   }*/
- //  return await view.render("pages/about/index")
-//})//.use(middleware.view)
-
-// Register all route modules
-/*router.use(homeRoutes)
-router.use(authRoutes)
-router.use(adminRoutes)
-router.use(merchantRoutes)
-router.use(customerRoutes)
-router.use(storeRoutes)
-router.use(productRoutes)
-router.use(categoryRoutes)
-router.use(cartRoutes)
-router.use(paymentRoutes)
-router.use(notificationRoutes)
-router.use(pagesRoutes)
-*/
+import "./routes/product.js"
+import "./routes/merchant.js"
+import "./routes/category.js"
